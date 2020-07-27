3d5e79b5e5d52dd936d253e73c961a0be91f56dd
Eduardo Cordeiro <eduardo.cordeiro1@outlook.com>
2020-07-27 18:24:49 -0300

    Pasta raiz organizada para começarmos o projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+
+export default function menu(){
+    return (
+        <nav>
+            <img src="{Logo}" alt="Logo Duflix"/>
+        </nav>
+    )
+}
